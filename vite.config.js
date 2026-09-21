41236c35fa705450bc133eaf0deee81a1e8e843b
paridhiyadav-001 <paridhiy517@gmail.com>
2026-09-21 15:14:45 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: './public', // Agar index.html public folder ke andar hai
-  build: {
-    outDir: '../dist',
-  },
-});
+})
